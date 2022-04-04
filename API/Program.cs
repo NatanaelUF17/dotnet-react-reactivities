20c1e54701360bf0d143ce841b11b2785ab17210
Natanael Urena <NatanaelUF17@gmail.com>
2022-04-03 21:52:26 -0400

    🚀 Updating content security policy

--- API/Program.cs
+++ API/Program.cs
@@ -43,7 +43,7 @@ app.UseCsp(options => {
         .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
         .FormActions(s => s.Self())
         .FrameAncestors(s => s.Self())
-        .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+        .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:"))
         .ScriptSources(s => s.Self());
 });
 
